4b0767c58665e2c127f1098ee9b1bd30d337f8f4
gustavo <gamendez98@gmail.com>
2024-02-15 20:19:36 -0500

    make thing work with batches

--- etl/extraction/semantic_scholar.py
+++ etl/extraction/semantic_scholar.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from typing import List
+
 import requests
 
 from etl.extraction.utils import initial_extraction, retry
@@ -13,7 +15,8 @@ SEMANTIC_SCHOLAR_FIELDS = ",".join(
 
 class SemanticURLs(str, Enum):
     SEARCH = 'https://api.semanticscholar.org/graph/v1/paper/autocomplete'
-    DETAILS = 'https://api.semanticscholar.org/graph/v1/paper/{paper_id}'
+    DETAILS = 'https://api.semanticscholar.org/graph/v1/paper/{paper_id}',
+    BATCH = 'https://api.semanticscholar.org/graph/v1/paper/batch'
 
 
 # %%
@@ -30,6 +33,15 @@ def details_request(idx: str):
 
 # %%
 
+@retry()
+def batch_request(ids: List[str]):
+    return requests.post(SemanticURLs.BATCH.value, params={'fields': SEMANTIC_SCHOLAR_FIELDS},
+                         json={"ids": ids}
+                         )
+
+
+# %%
+
 def semantic_scholar_search_id(title: str) -> str | None:
     search_result = id_request(title)
     if search_result.status_code == 404:
@@ -38,6 +50,15 @@ def semantic_scholar_search_id(title: str) -> str | None:
         matches = search_result.json().get('matches', [])
         if matches:
             return matches[0]['id']
+
+
+# %%
+
+def semantic_scholar_batch_details(ids: List[str]):
+    results = batch_request(ids)
+    if results.status_code != 200:
+        return []
+    return [result for result in results.json() if result]
 
 
 # %%
@@ -52,16 +73,15 @@ def semantic_scholar_details(idx: str) -> dict | None:
 
 # %%
 
-def semantic_scholar_entry_mapper(data_entry):
-    return semantic_scholar_details(semantic_scholar_search_id(data_entry['title']))
+def semantic_scholar_entry_mapper(data_entries):
+    ids = [semantic_scholar_search_id(data_entry['title']) for data_entry in data_entries]
+    ids = [i for i in ids if i]
+    return semantic_scholar_batch_details(ids)
 
 
 # %%
 
 if __name__ == '__main__':
-    initial_extraction(
-        'data/dict_split_3.json',
-        'data/extraction/semantic_scholar_visited.txt',
-        'data/extraction/semantic_scholar_results.json',
-        entry_mapper=semantic_scholar_entry_mapper)
+    initial_extraction('data/dict_split_3.json', 'data/extraction/semantic_scholar_visited.txt',
+                       'data/extraction/semantic_scholar_results.json', entry_mapper=semantic_scholar_entry_mapper)
     # TODO: extract information from citations, references and authors
--- etl/extraction/utils.py
+++ etl/extraction/utils.py
@@ -28,7 +28,20 @@ def retry(max_retries=7, base_delay=2.5, valid_codes=(200, 404)):
     return decorator
 
 
-def initial_extraction(input_file_path: str, visited_keys_path: str, output_path: str, entry_mapper: Callable):
+def batched(iterable, n=1, skip=None):
+    r = []
+    for ndx in iterable:
+        if skip and skip(ndx):
+            continue
+        r.append(ndx)
+        if len(r) == n:
+            yield r
+            r = []
+    yield r
+
+
+def initial_extraction(input_file_path: str, visited_keys_path: str, output_path: str, entry_mapper: Callable,
+                       batch_size: int = 30):
     '''
     Initial data extraction. Write results and entries already visited from input file
     :param input_file_path: the source provided by the teacher
@@ -38,18 +51,25 @@ def initial_extraction(input_file_path: str, visited_keys_path: str, output_path
     :param entry_mapper: function that takes the data provided by the teacher and returns the data provided by the API
     :return: None
     '''
+
+    def save_results(rs, ks, out, visited):
+        for result in rs:
+            if result:
+                out.write(json.dumps(result))
+                out.write('\n')
+        for k in ks:
+            visited.write(k)
+            visited.write('\n')
+
     with (open(input_file_path, 'r') as input_file,
           open(visited_keys_path, 'r') as visited_keys_file):
         visited_keys = {key.strip() for key in visited_keys_file.readlines()}
         initial_source = json.load(input_file)
     with (open(output_path, 'a') as output_file,
           open(visited_keys_path, 'a') as visited_keys_file):
-        for key, data in tqdm(initial_source.items(), total=len(initial_source), desc='retrieving semantic scholar'):
-            if key in visited_keys:
-                continue
-            result = entry_mapper(data)
-            if result:
-                output_file.write(json.dumps(result))
-                output_file.write('\n')
-            visited_keys_file.write(key)
-            visited_keys_file.write('\n')
+        batches = batched(initial_source.items(), n=batch_size, skip=(lambda x: x[0] in visited_keys))
+        for batch in tqdm(batches, total=int(len(initial_source) / batch_size), desc='retrieving semantic scholar'):
+            keys = [k for k, _ in batch]
+            datums = [d for _, d in batch]
+            results = entry_mapper(datums)
+            save_results(results, keys, output_file, visited_keys_file)

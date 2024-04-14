878c13f9e921974b0aac9141770676c8d32914b3
gustavo <gamendez98@gmail.com>
2024-04-14 13:33:54 -0500

    small fixes

--- etl/load/load_instances.py
+++ etl/load/load_instances.py
@@ -3,27 +3,27 @@ from urllib.parse import quote_plus
 
 from rdflib import Graph, Namespace, Literal, BNode
 from rdflib.namespace import RDF, RDFS, XSD
-from numpy import nan
+from numpy import isnan
 
 # %%
 import pandas as pd
-
-df = pd.read_hdf('semantic_web.h5', key='sw')
+from tqdm import tqdm
 
 # %%
 
-g = Graph()
 BASE = Namespace('http://example.org/')
 VOCAB = Namespace('http://example.org/vocab/')
-g.bind('ex', BASE)
-g.bind('exv', VOCAB)
-invalid_values = [None, "", nan, [], {}]
+INVALID_VALUES = [None, "", [], {}]
 
 
 # %%
 
 def text_to_node(text):
     return BASE.term(quote_plus(text))
+
+
+def is_valid(value):
+    return not ((type(value) == float and isnan(value)) or value in INVALID_VALUES)
 
 
 # %%
@@ -33,13 +33,15 @@ def connect_publications(graph, publication, row, connection_type):
         connection, inverse = (VOCAB.references, VOCAB.cites)
     else:
         connection, inverse = (VOCAB.cites, VOCAB.references)
-    if row[connection_type] not in invalid_values:
+    if is_valid(row[connection_type]):
         for connected_publication_info in row[connection_type]:
-            connected_publication = text_to_node(connected_publication_info['paperId'])
-            graph.add((connected_publication, RDF.type, VOCAB.Publication))
-            graph.add((connected_publication, VOCAB.hasSemanticId, Literal(connected_publication_info['paperId'])))
-            graph.add((publication, connection, connected_publication))
-            graph.add((connected_publication, inverse, publication))
+            paper_id = connected_publication_info.get('paperId')
+            if paper_id:
+                connected_publication = text_to_node(paper_id)
+                graph.add((connected_publication, RDF.type, VOCAB.Publication))
+                graph.add((connected_publication, VOCAB.hasSemanticId, Literal(paper_id)))
+                graph.add((publication, connection, connected_publication))
+                graph.add((connected_publication, inverse, publication))
 
 
 # %%
@@ -64,40 +66,54 @@ def load_publication_entry(graph: Graph, row):
     ]
     # simple properties
     for property, field in literal_properties:
-        if row[field] not in invalid_values:
+        if is_valid(row[field]):
             graph.add((publication, VOCAB.term(property), Literal(row[field])))
     # relationships between publications
     connect_publications(graph, publication, row, 'references')
     connect_publications(graph, publication, row, 'citations')
     # venue
-    if row.venue not in invalid_values:
+    if is_valid(row.venue):
         venue = text_to_node(row.venue.lower())
         graph.add((venue, RDF.type, VOCAB.Venue))
         graph.add((publication, VOCAB.belongsToVenue, venue))
     # categories
-    if row.category_string not in invalid_values:
+    if is_valid(row.categories):
         for category_string in row.categories:
             category = text_to_node(category_string.lower())
             graph.add((category, RDF.type, VOCAB.Category))
             graph.add((publication, VOCAB.belongsToCategory, category))
     # fields of study
-    if row.fieldsOfStudy not in invalid_values:
+    if is_valid(row.fieldsOfStudy):
         for field_of_study_string in row.fieldsOfStudy:
             field_of_study = text_to_node(field_of_study_string.lower())
             graph.add((field_of_study, RDF.type, VOCAB.FieldsOfStudy))
             graph.add((publication, VOCAB.belongsToFieldsOfStudy, field_of_study))
     # publication date
-    if row.hasPublicationDate not in invalid_values:
-        publication_date = strptime(row.hasPublicationDate, '%Y-%m-%d')
+    if is_valid(row.publicationDate):
+        publication_date = strptime(row.publicationDate, '%Y-%m-%d')
         graph.add((publication, VOCAB.hasPublicationDate, Literal(publication_date)))
     # updated on
-    if row.updated not in invalid_values:
-        updated_date = strptime(row.hasPublicationDate, '%Y-%m-%dT%H:%M:%S%z')
+    if is_valid(row.updated):
+        updated_date = strptime(row.updated, '%Y-%m-%dT%H:%M:%S%z')
         graph.add((publication, VOCAB.hasPublicationDate, Literal(updated_date)))
     # sections
-    if row.sections not in invalid_values:
+    if is_valid(row.sections):
         for section_title, section_body in row.sections.items():
             section = BNode()
             graph.add((section, RDF.type, VOCAB.Section))
             graph.add((section, VOCAB.hasSectionTitle, Literal(section_title)))
             graph.add((section, VOCAB.hasSectionBody, Literal(section_body)))
+
+
+def create_graph():
+    df: pd.DataFrame = pd.read_hdf('data/transform/semantic_web.h5', key='sw')
+    graph = Graph()
+    graph.bind('ex', BASE)
+    graph.bind('exv', VOCAB)
+    graph.parse('schema.ttl', format='ttl')
+    for _, row in tqdm(df.iterrows(), total=len(df)):
+        load_publication_entry(graph, row)
+
+
+if __name__ == '__main__':
+    create_graph()
